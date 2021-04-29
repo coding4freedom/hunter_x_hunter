b5003f6025cf61e04c6f2b86a911024d8a545853
Chris <chawkins2010@hotmail.com>
2021-04-28 20:17:30 -0500

    dashboard prep

---
+++ src/Components/Dashboard.jsx
@@ -0,0 +1,12 @@
+
+import styles from './dashboard.module.css'
+
+const Dashboard = () => {
+    return(
+        <div className={styles.base}>
+            
+        </div>
+    );
+}
+
+export default Dashboard;
